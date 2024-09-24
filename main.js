1171d4cd3f1e07e0735ad02b523b294dfe95ce8e
caioH12 <141171838+caioH12@users.noreply.github.com>
2024-09-24 11:46:42 -0300

    a

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function multiplicação(num1,num2){
     return num1*num2;
 }
 console.log(multiplicação(6,6));
-função areaquadrado(lado){
+function areaquadrado(lado){
     return lado**2;
 }
+console.log(areaquadrado(9))
