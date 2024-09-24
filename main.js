0c2f16f2574b7941b1a9dfd498e90b15a0dcf01d
caioH12 <141171838+caioH12@users.noreply.github.com>
2024-09-24 12:13:08 -0300

    kl

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ let areatrapezio= function(num1,num2,num3){
     return (num1+num2)*num3/2;
 }
 console.log(areatrapezio(2,3,5))
-let nome= prompt('nome?');
-document.write(nome);
-parseInt(prompt('nome?'))
+let nome= prompt('media?');
+document.write(media);
+parseInt(prompt('media?'))
 function media(num1,num2,num3,){
     return (num1+num2=num3)/num1+2+3;
 }
