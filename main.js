4f74601eb0f1d319a033c7de6f5dae171a4e6fb2
caioH12 <141171838+caioH12@users.noreply.github.com>
2024-10-01 11:46:26 -0300

    a

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-function soma(num1,num2){
-    return num1+num2;
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(2,3));
+// function multiplicação(num1,num2){
+//     return num1*num2;
+// }
+// console.log(multiplicação(6,6));
+// function areaquadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaquadrado(9))
+// let areatrapezio= function(num1,num2,num3){
+//     return (num1+num2)*num3/2;
+// }
+// console.log(areatrapezio(2,3,5))
+// let nome= prompt('media?');
+// document.write(media);
+// parseInt(prompt('media?'))
+// function media(num1,num2,num3,){
+//     return (num1+num2=num3)/num1+2+3;
+// }
+// console.log(media(3,4,6))
+
+let paises =['brasil', 'china', 'estados unidos', 'russia', 'egito', 'canada', 'italia', 'franca'];
+let i=0;
+while(i<7){
+    console.log(i);
+    i=i+1;
 }
-console.log(soma(2,3));
-function multiplicação(num1,num2){
-    return num1*num2;
-}
-console.log(multiplicação(6,6));
-function areaquadrado(lado){
-    return lado**2;
-}
-console.log(areaquadrado(9))
-let areatrapezio= function(num1,num2,num3){
-    return (num1+num2)*num3/2;
-}
-console.log(areatrapezio(2,3,5))
-let nome= prompt('media?');
-document.write(media);
-parseInt(prompt('media?'))
-function media(num1,num2,num3,){
-    return (num1+num2=num3)/num1+2+3;
-}
-console.log(media(3,4,6))
