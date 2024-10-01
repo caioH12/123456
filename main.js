2b626e858f3dd3a38bf20de23b329412ea9e9ec3
caioH12 <141171838+caioH12@users.noreply.github.com>
2024-10-01 12:13:56 -0300

    a

--- main.js
+++ main.js
@@ -22,15 +22,23 @@
 // }
 // console.log(media(3,4,6))
 
-let paises =['brasil', 'china', 'estados unidos', 'russia', 'egito', 'canada', 'italia', 'franca', 'inglaterra', 'coreia', 'alemanha'];
-let i=0;
-while(i<paises.length){
-    console.log(i);
-    console.log (paises[i]);
-    i=i+1;
-}
-for (let i = 0; i < paises.length; i++) {
-    console.log (i);
-    console.log(paises[i]);
+// let paises =['brasil', 'china', 'estados unidos', 'russia', 'egito', 'canada', 'italia', 'franca', 'inglaterra', 'coreia', 'alemanha'];
+// let i=0;
+// while(i<paises.length){
+//     console.log(i);
+//     console.log (paises[i]);
+//     i=i+1;
+// }
+// for (let i = 0; i < paises.length; i++) {
+//     console.log (i);
+//     console.log(paises[i]);
     
+// }
+let idade=15;
+if (idade>17) {
+    console.log('pode voar')
+    
+}
+else{
+console.log('ilegal voar')
 }
